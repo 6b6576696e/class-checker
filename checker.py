215c4b52eb3e49e6bd0a853b2830f45352d26503
Josh <jsoemant@uci.edu>
2021-08-16 21:43:41 -0700

    Added docstring and type hinting to the check class

--- checker.py
+++ checker.py
@@ -1,35 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
-import random
 
 
 YEAR_TERM = ""
 
 
 class Check:
-    def __init__(self, class_dict):
+    """
+    A class used to represent a web-scraper for WebSoc.
+    """
+    def __init__(self, class_dict: dict):
         self.headers = {'user-agent': 'Chrome/84.0.4147.105'}
         self.url = 'https://www.reg.uci.edu/perl/WebSoc'
         self.class_dict = class_dict
 
-    def check_courses(self):
+    def check_courses(self) -> dict:
+        """
+        Checks through the given courses and returns a dictionary of the open courses and their labs/discussions.
+        """
         open_courses = dict()
 
         for lec in self.class_dict:
-            if self.search_course(lec):
+            if self.check_availability(lec):
                 open_courses[lec] = []
                 for disc in self.class_dict[lec]:
-                    if self.search_course(disc):
+                    if self.check_availability(disc):
                         open_courses[lec].append(disc)
         return open_courses
 
-    def search_course(self, class_code):
+    def check_availability(self, class_code: str) -> bool:
+        """
+        Checks the availability of a class given the class code, returns a bool value
+        """
         params = {'CourseCodes': class_code, 'YearTerm': YEAR_TERM, 'Submit': 'XML'}
         request = requests.get(self.url, params=params, headers=self.headers)
         page = BeautifulSoup(request.content, 'lxml')
-        return self.check_course(class_code, page)
-
-    def check_course(self, class_code, page):
         if '/' in page.find('sec_enrolled').text:
             enrolled, max_enroll = [i.strip() for i in page.find('sec_enrolled').text.split('/')]
         else:
